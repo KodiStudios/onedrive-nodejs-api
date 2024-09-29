b4d27b45e73b283fbbde31549ba164e7cfad70e7
Nikolai Aristov <KodiStudios@gmail.com>
2024-09-29 13:01:28 -0700

    Me Api

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client } = require("@microsoft/microsoft-graph-client");
+//require("isomorphic-fetch");
+
+const graphClient = Client.init({
+  defaultVersion: "v1.0",
+  debugLogging: true,
+  authProvider: (done) => {
+    const errorMessage = "error throw by the authentication handler";
+    done(
+      errorMessage,
+      "TOKEN_HERE"
+    );
+  },
+});
+
+graphClient
+  .api("/me")
+  .select("displayName")
+  .get()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
