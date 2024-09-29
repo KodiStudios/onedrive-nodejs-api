254d1f52e0eac72d8d709cdcad606f63a5b0795e
Nikolai Aristov <KodiStudios@gmail.com>
2024-09-29 13:19:39 -0700

    Add Token Parsing

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const { Client } = require("@microsoft/microsoft-graph-client");
+const argv = require("minimist")(process.argv.slice(2));
+
+if (!argv.token) {
+  console.log("Usage: ");
+  console.log("node index.js --token {token_value_from_aka.ms/ge}");
+  return;
+}
 
 const graphClient = Client.init({
   defaultVersion: "v1.0",
   debugLogging: true,
   authProvider: (done) => {
     const errorMessage = "error throw by the authentication handler";
-    done(
-      errorMessage,
-      "TOKEN_HERE"
-    );
+    done(errorMessage, argv.token);
   },
 });
 
